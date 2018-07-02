f60b6e25a9bc80b12a7c109fca877df8a1ff83db
BAIREN ZHU <38541450+BairenZhu@users.noreply.github.com>
2018-07-02 16:25:50 -0400

    Update Group2.java

--- Group2.java
+++ Group2.java
@@ -3,109 +3,133 @@ package Database;
 
 import java.sql.*;
 
-
-public class Group2 {
-
-	public static void main(String args[]) throws Exception {
-			//connect to database and ready to modify it
-		Connection conn = getConnection(); 
-		conn.setAutoCommit(false);
-		conn.setTransactionIsolation(Connection.TRANSACTION_SERIALIZABLE); 
-		Statement stmt=null;
-		 ResultSet rs = null;
-         ResultSet rs1 = null;
-         ResultSet rs2 = null;
-         ResultSet rs3 = null;
-		try {
-			 stmt = conn.createStatement();
-			stmt.executeUpdate("DROP TABLE IF EXISTS Stock CASCADE");
+public class Group2{
+    public static void main(String[] args)throws SQLException, ClassNotFoundException {
+        	//Load the MySQL driver
+    	
+        	String driver="com.mysql.jdbc.Driver";
+            Class.forName(driver);
+                
+            String url="jdbc:mysql://localhost:3306/Project"; 
+            
+            //Get a connection to database
+            Connection conn;
+            conn = DriverManager.getConnection(url,"root","12332321");
+         	
+            // For Auto
+    		conn.setAutoCommit(false);
+    		
+    		// For Isolation 
+    	    conn.setTransactionIsolation(Connection.TRANSACTION_SERIALIZABLE); 
+    		
+            Statement stmt = null;
+            
+            ResultSet rs = null;
+            ResultSet rs2 = null;
+            ResultSet rs3 = null;
+            ResultSet rs4 = null;
+            
+            try {
+            //Create statement object
+            stmt = conn.createStatement(); 
+            
+            stmt.executeUpdate("DROP TABLE IF EXISTS Stock CASCADE");
 			stmt.executeUpdate("DROP TABLE IF EXISTS Product CASCADE");
 			stmt.executeUpdate("DROP TABLE IF EXISTS Depot CASCADE");
-			
-			//Create Table
+            
+            //Create Table
 			stmt.executeUpdate("Create table IF NOT EXISTS Product("
-					+ "prod_id CHAR(10),"+ "pname VARCHAR(30),"+ "price DECIMAL,"+ "PRIMARY KEY (prod_id),"
-					+ "CHECK (price > 0)"+ ")");
+					+ "prod_id CHAR(10),"
+					+ "pname VARCHAR(128),"
+					+ "price DECIMAL,"
+					+ "PRIMARY KEY (prod_id),"
+					+ "CHECK (price > 0)"
+					+ ")");
 			stmt.executeUpdate("Create table IF NOT EXISTS Depot("
-					+ "dep_id CHAR(10),"+ "addr VARCHAR(30),"+ "volume INTEGER,"
-					+ "PRIMARY KEY (dep_id),"+ ")");
+					+ "dep_id CHAR(10),"
+					+ "addr VARCHAR(128),"
+					+ "volume INTEGER,"
+					+ "PRIMARY KEY (dep_id),"
+					+ "CHECK (volume >= 0)"
+					+ ")");
 			stmt.executeUpdate("Create table IF NOT EXISTS Stock("
-					+ "prod_id CHAR(10),"+ "dep_id CHAR(10),"+ "quantity INTEGER,"
+					+ "prod_id CHAR(10),"
+					+ "dep_id CHAR(10),"
+					+ "quantity INTEGER,"
 					+ "PRIMARY KEY (prod_id, dep_id),"
 					+ "FOREIGN KEY (prod_id) REFERENCES Product (prod_id) ON UPDATE CASCADE," 
-					+ "FOREIGN KEY (dep_id) REFERENCES Depot (dep_id) ON UPDATE CASCADE"+ ")");
+					+ "FOREIGN KEY (dep_id) REFERENCES Depot (dep_id) ON UPDATE CASCADE"
+					+ ")");
 			
 			stmt.executeUpdate("INSERT INTO Product (prod_id, pname, price) Values" 
-					+ "('p1', 'tape', 2.5)," + "('p2', 'tv', 250), "+ "('p3', 'vcr', 80);");
+					+ "('p1', 'tape', 2.5)," 
+					+ "('p2', 'tv', 250), "
+					+ "('p3', 'vcr', 80);");
 			stmt.executeUpdate("INSERT INTO Depot (dep_id, addr, volume) Values" 
-					+ "('d1', 'New Yrok', 9000)," + "('d2', 'Syracuse', 6000), "+ "('d4', 'New Yrok', 2000);");
+					+ "('d1', 'New Yrok', 9000)," 
+					+ "('d2', 'Syracuse', 6000), "
+					+ "('d4', 'New Yrok', 2000);");
 			stmt.executeUpdate("INSERT INTO Stock (prod_id, dep_id, quantity) Values" 
-					+ "('p1', 'd2', -100)," + "('p1', 'd4', 1200)," + "('p3', 'd1', 3000)," + "('p3', 'd4', 2000)," 
-					+ "('p2', 'd4', 1500)," + "('p2', 'd1', -400)," + "('p2', 'd2', 2000);");
-
-			//output before update
-			System.out.println("before Update");
+					+ "('p1', 'd2', -100)," 
+					+ "('p1', 'd4', 1200)," 
+					+ "('p3', 'd1', 3000)," 
+					+ "('p3', 'd4', 2000)," 
+					+ "('p2', 'd4', 1500)," 
+					+ "('p2', 'd1', -400)," 
+					+ "('p2', 'd2', 2000);");
+			
+            //Before Update
+			System.out.println("Before Update:");
+			
 			rs = stmt.executeQuery("select * from Depot");
-			System.out.println(" Depot");
-			System.out.println("dep_Id  " + "addr " + " volume ");
-			while(rs.next()) {
-				System.out.println( rs.getString("dep_Id ")  + "\t " + rs.getString("addr") + "\t " + rs.getInt("volume"));
-			} 
-			
-			rs1 = stmt.executeQuery("select * from Stock");
-			System.out.println("\n Stock");
-			System.out.println("Prod_Id  " + "Dep_Id " + " Quantity ");
-			while(rs1.next()) {
-				System.out.println(rs1.getString("Prod_Id") + "\t " + rs1.getString("Dep_Id") + "\t " + rs1.getInt("quantity"));
-			} 
-			
-			
-			//output after update
-			System.out.println("\nAfter Update");
-			//our operations
-			stmt.executeUpdate("DELETE FROM Stock WHERE dep_id='d1'");
-			stmt.executeUpdate("DELETE FROM Depot WHERE dep_id = 'd1'");
-			
-			 rs2 = stmt.executeQuery("select * from Depot");
 			System.out.println("Depot");
-			System.out.println("dep_Id  " + "addr " + " volume ");
-			while(rs2.next()) {
-				System.out.println( rs2.getString("dep_Id") + "\t " + rs2.getString("addr") + "\t " + rs2.getInt("volume"));
-			} 
-			
-			rs3 = stmt.executeQuery("select * from Stock");
-			System.out.println("\n Stock");
+			System.out.println("Dep_id  " + " Addr   " + "       Volume");
+				while(rs.next()) {
+					System.out.println(rs.getString("Dep_id") + "\t " +  rs.getString("Addr") + "\t " + rs.getInt("Volume"));
+				} 
+				
+		    
+            rs2 = stmt.executeQuery("select * from Stock");
+			System.out.println("\nStock");
 			System.out.println("Prod_Id  " + "Dep_Id " + " Quantity ");
-			while(rs3.next()) {
-				System.out.println(rs3.getString("Prod_Id") + "\t " + rs3.getString("Dep_Id") + "\t " + rs3.getInt("quantity"));
-			} 
+				while(rs2.next()) {
+					System.out.println(rs2.getString("Prod_Id") + "\t "+ rs2.getString("Dep_Id") + "\t " + rs2.getInt("Quantity"));
+				} 
+				
+			//After Update
+			System.out.println("\nAfter Update:");
 			
-		} catch (Exception e) {
-			System.out.println( e);
-			conn.rollback();
-			stmt.close();
-			conn.close();
-			return;
-		} 
-		System.out.println("Function complete");
-		conn.commit();
-		stmt.close();
-		conn.close();
-	}
-	
-	public static Connection getConnection() throws Exception{
-		try {
-		String driver="com.mysql.jdbc.Driver";
-		String url="jdbc:mysql://localhost:3306/Project";
-		Class.forName(driver);
-		
-		Connection conn = DriverManager.getConnection(url, "root", "12332321");
-	
-		System.out.println("Connected");
-		return conn;
-		}catch (Exception e) {
-			System.out.println(e);
-		}
-		return null;
-	}
-}
+			//Group 2 : delete  dep_id = 'd1' from both Depot and Stock
+			//stmt.executeUpdate("DELETE FROM Depot WHERE dep_id = 'd1'");   //Isolation
+			stmt.executeUpdate("DELETE FROM Stock WHERE dep_id='d1'");		
+			stmt.executeUpdate("DELETE FROM Depot WHERE dep_id = 'd1'"); 
+			//stmt.executeUpdate("DELETE FROM Depot WHERE prod_id = 'd1'");  //Auto
+			
+			rs3 = stmt.executeQuery("select * from Depot");				
+			System.out.println("Depot");
+			System.out.println("Dep_id  " + "  Addr    " + "     Volume");
+				while(rs3.next()) {
+					System.out.println( rs3.getString("dep_id") + "\t " +  rs3.getString("addr") + "\t " + rs3.getInt("volume"));
+				} 
+				
+				
+			rs4 = stmt.executeQuery("select * from Stock");
+			System.out.println("\nStock");
+			System.out.println("prod_Id  " + "dep_Id " + " quantity ");
+				while(rs4.next()) {
+					System.out.println(rs4.getString("Prod_Id") + "\t "+ rs4.getString("Dep_Id") + "\t " + rs4.getInt("Quantity"));
+				} 
+                
+
+            }catch (Exception e) {
+    			System.out.println( e);
+    			conn.rollback();
+    			stmt.close();
+    			conn.close();
+    			return;
+    		} 
+    		conn.commit();
+    		stmt.close();
+    		conn.close();
+    	}
+    }
